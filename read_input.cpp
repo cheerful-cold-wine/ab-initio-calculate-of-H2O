9b74e76fb94e4c6be73498cabeb178b95ba22d7d
lrc <2691936627@qq.com>
2023-12-26 03:04:33 +0800

    1

--- read_input.cpp
+++ read_input.cpp
@@ -2,15 +2,37 @@
 #include<fstream>
 #include<filesystem>
 #include<string>
+#include<vector>
+
 using namespace std;
+
+vector<string> read_file(string file_name){
+    vector<string> file_str;
+    string pwd = filesystem::current_path();
+    ifstream file;
+    file.open(pwd+"/input/"+file_name,ios::in);
+    if(file.is_open()){
+        string line;
+        while(getline(file,line)){
+            file_str.push_back(line);
+        }
+        file.close();
+    } else {
+        cerr<< "unable to open file "<< file_name << endl;
+    }
+    return file_str;
+}
 
 int main()
 {
-    string pwd = filesystem::current_path();
-    ifstream basis;
-    basis.open(pwd+"/input/basis",ios::in);
-    if(basis.is_open()){
-        
+    vector<string> basis_str = read_file("basis");
+    vector<string> structure_str = read_file("structure.xyz");
+    
+    for(const auto& str : basis_str){
+        cout<< str<<endl;
+    }
+    for(const auto& str : structure_str) {
+        cout<< str <<endl;
     }
     return 0;
 }
