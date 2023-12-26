0fa6273bda8d61e484476790a8aaa0c06b92ad55
lrc <2691936627@qq.com>
2023-12-26 18:11:29 +0800

    1

--- read_input.cpp
+++ read_input.cpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include<filesystem>
 #include<string>
+#include<sstream>
 #include<vector>
 
 using namespace std;
@@ -23,16 +24,33 @@ vector<string> read_file(string file_name){
     return file_str;
 }
 
+vector<string> split_str_by_space(string str){
+    vector<string> result;
+    istringstream iss(str);
+    for (string word; iss>>word;){
+        result.push_back(word);
+    }
+    return result;
+}
+
 int main()
-{
+{   
     vector<string> basis_str = read_file("basis");
+    for (string str : basis_str){
+        if (str=="****") continue;
+        vector<string> str_= split_str_by_space(str);
+        for (int i=0; i<str_.size(); i++){
+            cout<< str_[i]<<endl;
+        }
+    }
     vector<string> structure_str = read_file("structure.xyz");
-    
+/*    
     for(const auto& str : basis_str){
         cout<< str<<endl;
     }
     for(const auto& str : structure_str) {
         cout<< str <<endl;
     }
+*/
     return 0;
 }
